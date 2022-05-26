0590ede77ebd38e9595e29077b6b34032d3c4f7d
LAPTOP-LFMCA6CQ\Duan <duvanale2016@gmail.com>
2022-05-26 14:28:22 -0500

    migration and model document type

---
+++ database/migrations/2022_05_26_192108_create_document_type_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('document_type', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('uuid', 36)->nullable();
+            $table->string('name', 25);
+            $table->boolean('status')->nullable()->default(true);
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('update_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('document_type');
+    }
+}
