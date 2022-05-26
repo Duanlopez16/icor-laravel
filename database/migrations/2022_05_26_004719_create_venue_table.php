d5eaeef977ca68d05e02b9dbfa3fffcb5b225d54
LAPTOP-LFMCA6CQ\Duan <duvanale2016@gmail.com>
2022-05-25 20:03:00 -0500

    add uuid composer and model venue

---
+++ database/migrations/2022_05_26_004719_create_venue_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVenueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venue', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('uuid', 36);
+            $table->string('name', 245);
+            $table->string('address', 254);
+            $table->integer('phone');
+            $table->integer('city_id');
+            $table->double('latitud');
+            $table->double('longitud');
+            $table->boolean('status')->nullable()->default(true);
+            $table->dateTime('created_at');
+            $table->dateTime('update_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venue');
+    }
+}
